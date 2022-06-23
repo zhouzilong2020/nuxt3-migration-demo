5342ba45d684ad8bc89ae2cef77b178a0ca71b54
Zilong Zhou <529620861@qq.com>
2022-06-23 11:24:44 +0800

    add naive-ui

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,18 @@
-import { defineNuxtConfig } from 'nuxt'
+import { defineNuxtConfig } from "nuxt";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
-})
+  build: {
+    transpile: [
+      "naive-ui",
+      "vueuc",
+      "@css-render/vue3-ssr",
+      "@juggle/resize-observer",
+    ],
+  },
+  vite: {
+    optimizeDeps: {
+      include: ["date-fns-tz/esm/formatInTimeZone"],
+    },
+  },
+});
---
+++ pages/index.vue
@@ -0,0 +1,3 @@
+<template>
+  <nuxt-link to="/naive-ui">naive-ui</nuxt-link>
+</template>
